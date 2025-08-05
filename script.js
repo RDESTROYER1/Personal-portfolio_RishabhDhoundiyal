6130f7098ab3e6f5bab218b39103d5a59dbdefd7
Rishabh Dhoundiyal <165043134+RDESTROYER1@users.noreply.github.com>
2025-08-05 13:06:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var typed = new Typed('#element', {
+    strings: ['Learner', 'Tech Enthusiast', 'Web Developer'],
+    typeSpeed: 50,
+});
+
+
+document.getElementById("githubBtn").addEventListener("click", function () {
+    window.open("https://github.com/RDESTROYER1", "_blank");
+});
