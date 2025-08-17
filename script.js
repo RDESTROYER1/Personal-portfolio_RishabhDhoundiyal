7cd74f942dc99979a4773ba7087b40bb5775710e
Rishabh Dhoundiyal <165043134+RDESTROYER1@users.noreply.github.com>
2025-08-17 13:42:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ var typed = new Typed('#element', {
     typeSpeed: 50,
 });
 
-
+// GitHub button
 document.getElementById("githubBtn").addEventListener("click", function () {
     window.open("https://github.com/RDESTROYER1", "_blank");
 });
+
+// Professional Highlights button → scroll to Projects section
 document.querySelector('.bto').addEventListener('click', () => {
     document.querySelector('.secondsection').scrollIntoView({ 
         behavior: 'smooth' 
     });
 });
+
+// Certificate buttons
+document.querySelectorAll('.btk').forEach(button => {
+    button.addEventListener('click', () => {
+        const link = button.getAttribute('data-link');
+        window.open(link, '_blank'); // opens in new tab
+    });
+});
+
+
+
 
